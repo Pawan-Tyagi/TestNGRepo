af4273d2290b6beb876a567dff466866e4821637
Niranjan <niranjan@concret.io>
2017-08-21 14:43:42 +0530

    Commit 21.08.2017

--- src/com/testcases/CreateAccount.java
+++ src/com/testcases/CreateAccount.java
@@ -12,63 +12,65 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.ITestResult;
-import org.testng.Reporter;
 import org.testng.annotations.AfterMethod;
 import com.testcases.SauceLabConfiguration;
 
 public class CreateAccount {
-	
+
 	static WebDriver driver;
-	public static final String URL = "https://" + SauceLabConfiguration.USERNAME + ":" + SauceLabConfiguration.ACCESS_KEY
+	public static final String URL = "https://"
+			+ SauceLabConfiguration.USERNAME + ":"
+			+ SauceLabConfiguration.ACCESS_KEY
 			+ "@ondemand.saucelabs.com:443/wd/hub";
 
 	@Test
 	public static void testng() throws InterruptedException,
 			MalformedURLException {
-		//configuring the sauce lab capabilities 
+		// configuring the sauce lab capabilities
 		DesiredCapabilities caps = DesiredCapabilities.chrome();
 		caps.setCapability("platform", "Windows 10");
 		caps.setCapability("name", "Creating Account and pushing it to DSM");
 		caps.setCapability("version", "59.0");
-		
-		//creating the remote driver
+
+		// creating the remote driver
 		driver = new RemoteWebDriver(new URL(URL), caps);
-		
-		//opening the browser in sauce lab
+
+		// opening the browser in sauce lab
 		driver.get("https://login.salesforce.com/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(50L, TimeUnit.SECONDS);
-		
-		//input the user credential
+
+		// input the user credential
 		driver.findElement(By.xpath("//*[@id='username']")).sendKeys(
 				SauceLabConfiguration.SF_USERNAME);
 		driver.findElement(By.xpath("//*[@id='password']")).sendKeys(
 				SauceLabConfiguration.SF_PASSWORD);
-		//submit the login button
+		// submit the login button
 		driver.findElement(By.xpath("//*[@id='Login']")).click();
-		
+
 		Thread.sleep(10000);
 		driver.findElement(By.xpath("//*[@id='AllTab_Tab']/a/img")).click();
-		//Thread.sleep(2000);
+		// Thread.sleep(2000);
 		driver.findElement(
 				By.xpath("//*[@id='bodyCell']/div[3]/div[2]/table/tbody/tr[1]/td[1]/a"))
 				.click();
-		//Thread.sleep(3000);
+		// Thread.sleep(3000);
 
 		driver.findElement(
 				By.xpath("//*[@id='hotlist']/table/tbody/tr/td[2]/input"))
 				.click();
-		driver.findElement(By.xpath("//*[@id='acc2']")).sendKeys("Automated Account 27");
+		driver.findElement(By.xpath("//*[@id='acc2']")).sendKeys(
+				"Automated Account 104");
 		driver.findElement(By.xpath("//*[@id='topButtonRow']/input[1]"))
 				.click();
 		driver.findElement(By.xpath("//*[@id='topButtonRow']/input[6]"))
 				.click();
-		//Thread.sleep(3000);
+		// Thread.sleep(3000);
 
 		Alert alert = driver.switchTo().alert();
 		alert.accept();
-		Thread.sleep(4000);//*[@id='identifier']  //*[@id='identifier']
-		
+		Thread.sleep(4000);// *[@id='identifier'] //*[@id='identifier']
+
 		driver.findElement(
 				By.xpath("//*[@id='pg:j_id1:j_id27:j_id40:bottom']/input"))
 				.click();
@@ -76,7 +78,7 @@ public class CreateAccount {
 		String ParentWindowHandle = driver.getWindowHandle();
 		Thread.sleep(5000);
 		driver.findElement(By.xpath("//*[@id='00N7F000004I6L1_ileinner']/a"))
-				.click();//*[@id='00N7F000004I6L1_ileinner']/a
+				.click();// *[@id='00N7F000004I6L1_ileinner']/a
 
 		Thread.sleep(3000);
 
@@ -96,113 +98,30 @@ public class CreateAccount {
 		driver.findElement(By.xpath("//*[@id='passwordNext']/content/span"))
 				.click();
 		Thread.sleep(4000);
-		driver.findElement(By.id("knowledge-preregistered-email-response")).sendKeys("matt@10Kview.com");
-		Thread.sleep(40000);
-		/*driver.switchTo().window(ParentWindowHandle);
-		
-		driver.findElement(By.xpath("//*[@id='setupLink']")).click();
-		Thread.sleep(3000);
-		driver.findElement(By.xpath("//*[@id='DevToolsIntegrate_icon']/img"))
-				.click();
-		driver.findElement(By.xpath("//*[@id='CustomSettings_font']")).click();
-		Thread.sleep(3000);
-		driver.findElement(
-				By.xpath("//*[@id='CS_Defn_List:theTemplate:panelGrid']/tbody/tr/td/div[5]/div/div[2]/table/tbody/tr[4]/td[1]/a"))
-				.click();
-		Thread.sleep(2000);
-		driver.findElement(
-				By.xpath("//*[@id='CS_list:CS_Form:theDetailPageBlock:thePageBlockButtons:edit']"))
-				.click();
-		Thread.sleep(2000);
-		driver.findElement(
-				By.xpath("//*[@id='CS_Edit:CS_Form:thePageBlock:thePageBlockSection:goog_dclk_dsm__DC_AllowAccountTriggerPushToDSM__c']"))
-				.click();
-		Thread.sleep(2000);
-		driver.findElement(
-				By.xpath("//*[@id='CS_Edit:CS_Form:thePageBlock:thePageBlockButtons:save']"))
-				.click();
-		Thread.sleep(2000);*/
-
-		/*driver.findElement(By.xpath("//*[@id='phSearchInput']")).sendKeys(
-				"Auto acc8");
+		/*driver.findElement(By.id("knowledge-preregistered-email-response"))
+				.sendKeys("matt@10Kview.com");*/
 		Thread.sleep(8000);
 
-		WebElement element = driver.findElement(By
-				.xpath("//*[@id='phSearchInput']"));
-		element.sendKeys(Keys.ENTER);
-		Thread.sleep(4000);
-
-		driver.findElement(
-				By.xpath("//*[@id='Account_body']/table/tbody/tr[2]/th/a"))
-				.click();
-		Thread.sleep(2000);
-		Actions action = new Actions(driver);
-		WebElement element1 = driver.findElement(By
-				.xpath("//*[@id='acc2_ileinner']"));
-
-		// Double click
-		action.doubleClick(element1).perform();
-		driver.findElement(By.xpath("//*[@id='acc2']")).sendKeys("Changed acc");
-		Thread.sleep(3000);
-
-		driver.findElement(By.xpath("//*[@id='topButtonRow']/input[1]"))
-				.click();
-		Thread.sleep(2000);
-		String ParentWindowHandle = driver.getWindowHandle();
-
-		driver.findElement(By.xpath("//*[@id='00N7F000002IVl7_ileinner']/a"))
-				.click();
+		System.out.println("Name of the Company:  "+driver.findElement(
+				By.xpath("//*[@id='gwt-debug-companyDetails-header']/table/tbody/tr/td[2]/div[2]/span/strong"))
+				.getText());
 		
-		Thread.sleep(6000);
-
-		for (String childTab : driver.getWindowHandles()) {
-			driver.switchTo().window(childTab);
-
-		}
-		
-		String str = driver.findElement(By.xpath("//*[@id='gwt-debug-companyDetails-header']/table/tbody/tr/td[2]/div[2]/span/strong")).getText();
-		Thread.sleep(2000);
-		
-		//assertEquals(str, "Changed acc");
-		driver.switchTo().window(ParentWindowHandle);
-		
-		driver.findElement(By.xpath("//*[@id='setupLink']")).click();
-		Thread.sleep(3000);
-		driver.findElement(By.xpath("//*[@id='DevToolsIntegrate_icon']/img"))
-				.click();
-		driver.findElement(By.xpath("//*[@id='CustomSettings_font']")).click();
-		Thread.sleep(3000);
-		driver.findElement(
-				By.xpath("//*[@id='CS_Defn_List:theTemplate:panelGrid']/tbody/tr/td/div[5]/div/div[2]/table/tbody/tr[4]/td[1]/a"))
-				.click();
-		Thread.sleep(2000);
-		driver.findElement(
-				By.xpath("//*[@id='CS_list:CS_Form:theDetailPageBlock:thePageBlockButtons:edit']"))
-				.click();
-		Thread.sleep(2000);
-		driver.findElement(
-				By.xpath("//*[@id='CS_Edit:CS_Form:thePageBlock:thePageBlockSection:goog_dclk_dsm__DC_AllowAccountTriggerPushToDSM__c']"))
-				.click();
-		Thread.sleep(2000);
-		driver.findElement(
-				By.xpath("//*[@id='CS_Edit:CS_Form:thePageBlock:thePageBlockButtons:save']"))
-				.click();
-		Thread.sleep(2000);
-*/
-		
-		String str = driver.findElement(By.xpath("///*[@id='bodyCell']/div[1]/div/div/h2"))
+		String str = driver
+				.findElement(
+						By.xpath("//*[@id='gwt-debug-companyDetails-header']/table/tbody/tr/td[2]/div[2]/span/strong"))
 				.getText();
-		//verifying the result
-		assertEquals(str, "Create New DSM Company From Salesforce Account");
-		//ITestResult result = Reporter.getCurrentTestResult();
+		// verifying the result
+		assertEquals(str, "Automated Account 104");
+		// ITestResult result = Reporter.getCurrentTestResult();
 
-}
+	}
+
 	@AfterMethod
 	/*
 	 * 
 	 * 
-	   showing the result status on sauce lab 
-	*/
+	 * showing the result status on sauce lab
+	 */
 	public static void afterMethod(ITestResult result) {
 		try {
 			JavascriptExecutor js = (JavascriptExecutor) driver;
@@ -219,4 +138,3 @@ public class CreateAccount {
 		}
 	}
 }
-
--- src/com/testcases/SauceLabConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.testcases;
-
-public class SauceLabConfiguration 
-{
-	public static final String USERNAME = "Ravi09";
-	public static final String ACCESS_KEY = "3868fce6-3dbf-4c23-8029-d1e53b416b5b";
-	public static final String SF_USERNAME = "testgooglemultiorg@10kview.com";
-	public static final String SF_PASSWORD = "jaipur123";
-}
